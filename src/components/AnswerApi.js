1162dfbc35acc10aa3df048aff97d9828f6c72a3
Tatiana Perez <tjperez@ncsu.edu>
2021-04-03 13:56:14 -0400

    dummy data for answers

---
+++ src/components/AnswerApi.js
@@ -0,0 +1,9 @@
+
+const dataAnswers = [ 
+    {body: 'answer1', author: 'author1', question: 'question1'},
+    {body: 'answer2', author: 'author2', question: 'question2'},
+    {body: 'answer3', author: 'author3', question: 'question3'},
+]
+
+
+export default dataAnswers
