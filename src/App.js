003eac0dc2e334410df56e037a956282473626c7
Albany <albany.prince@gmail.com>
2021-04-03 14:05:37 -0400

    login and register links work on navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import About from './components/About'
 import Nav from './components/Nav'
 import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
 import GenreQuestions from './components/GenreQuestions'
 import {
   BrowserRouter as Router,
@@ -21,6 +23,8 @@ function App() {
         <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About}  />
+        <Route path="/Login" component={Login}/>
+        <Route path="/Register" component={Register}/>
         <Route path="/GenreQuestions" component={GenreQuestions} />
         
         </Switch>   
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
+
+function Register() {
+    return (
+        <div>
+            <h1>Register Here</h1>
+        </div>
+    )
+}
+
+export default Register
