7c17ba999202abca742abb298004a00b82c08d59
Tatiana Perez <tjperez@ncsu.edu>
2021-04-03 19:44:21 -0400

    data list longer in prep for css

--- src/components/Categoriesapi.js
+++ src/components/Categoriesapi.js
@@ -2,6 +2,11 @@ const data = [
     {genre: 'Country', id: 1},
     {genre: 'Folk', id: 2},
     {genre: 'Opera', id: 3},
+    {genre: 'Rock', id: 1},
+    {genre: 'R&B', id: 2},
+    {genre: 'Pop', id: 3}, {genre: 'Country', id: 1},
+    {genre: 'Christmas', id: 2},
+    {genre: 'Indie', id: 3},
 
 ]
 
