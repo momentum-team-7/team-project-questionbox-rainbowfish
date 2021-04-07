4af1d6b21fe68a788d4eb3f1e6dce566cd64f5de
Tatiana Perez <tjperez@ncsu.edu>
2021-04-06 23:47:11 -0400

    registration form, still not working

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Link, Redirect } from 'react-router-dom'
 
-function Register() {
+export default function Registration({ isLoggedIn, setAuth }) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    if (isLoggedIn) {
+        return <Redirect to='/' />
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        axios
+            .post('http://questionbox-torpedo-shark.herokuapp.com/auth/users/', {
+                username,
+                password,
+            })
+            .then((response) => {
+                return axios 
+                .post('http://questionbox-torpedo-shark.herokuapp.com/auth/token/login', {
+                    username,
+                    password,
+                })
+                .then((data) => {
+                    if (data && data.data.auth_token) {
+                        setAuth(username, data.data.auth_token)
+                    }
+                })
+            })
+    }
+
     return (
         <div>
-            <h1>Register Here</h1>
+            <p>Register Here:</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label to='username'>Username:</label>
+                    <input 
+                    type='text'
+                    id='username'
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label>Password:</label>
+                    <input  
+                    type='password'
+                    id='password'
+                    required
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+
+
+                <button type='submit'>Register</button>
+            </form>
+            
         </div>
     )
 }
-
-export default Register
