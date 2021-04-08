308688a9ec40a1f3c01d4f9440bcf20e014dc9a2
Tatiana Perez <tjperez@ncsu.edu>
2021-04-08 00:18:41 -0400

    style style style

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,9 @@ function About() {
         <div>
             <h1 className="about">About</h1>
             <p>This is a totally awesome website that you should use and tell all your friends about. Follow us on instagram!!!!!! </p>
+
+
+        <p>Created by: <h1 className="team-name">Team Mega-Rainbow Torpedo SharkFish</h1></p>
         </div>
     )
 }
