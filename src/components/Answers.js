bc0329a7946a9d76ee81e2e45fd0f93cab7ea837
Tatiana Perez <tjperez@ncsu.edu>
2021-04-07 21:58:59 -0400

    help

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -12,58 +12,27 @@ function QuestionAnswers( props ) {
     const [reply, setReply] = useState(false)
     const {selectedQuestion} = props.location.state
     const [selectedUser, setSelectedUser] = useState(null)
-  
+
     useEffect(() => {
         axios.get('https://questionbox-torpedo-shark.herokuapp.com/answers/').then((response) => {
-            
             setAnswers(response.data)
-            
-            
-        
         })}, [])
+
         let answerstoRender 
         if (!lodash.isEmpty(answers)) {
-            // answerstoRender = answers.events.map(answer => {
             answerstoRender = Object.keys(answers).map((key) => {
-
-            console.log(answers[key])
             return <div className='answer-box' >
                 <div className="answer-content">
                 <h2>{answers[key].body}</h2>
-                
                 <Link onClick={() =>setSelectedUser()} 
                     className="answer-author" 
                     to={{ pathname: `/UserProfile/`, state: {selectedUser: answers[key].author.username, userAnswer: answers[key].body } }}>
                         <h3>{answers[key].author.username}</h3>
                 </Link>
-                <LikeBtn />
+                {/* <LikeBtn /> */}
                 </div>
                 </div>
-                
-        
-            })
-            }
-    // let answerstoRender 
-    // if (!lodash.isEmpty(answers.events)) {
-    //     answerstoRender = answers.events.map(answer => {
-            
-    //         console.log('answers', answers)
-    //         console.log('answer', answer)
-    //     return <div className='answer-box' >
-
-    //         <div className="answer-content">
-    //         <h2>{answer.body}</h2>
-    //        <Link onClick={() =>setSelectedUser()} className="answer-author" to={{ pathname: `/UserProfile/`, state: {selectedUser: answer.author, userAnswer: answer.body } }}><h3>{answer.author}</h3></Link>
-            
-    //         <LikeBtn />
-            
-    //         </div>
-
-    //         </div>
-            
-    
-    //     })
-    //     }
+            })}
 
     return (
         <div>
@@ -71,33 +40,13 @@ function QuestionAnswers( props ) {
                 <h1>{selectedQuestion.title}</h1>
                 <h4>{selectedQuestion.author.username}</h4>
                 
-
-
                 <InputField reply={reply} setReply={setReply}/> 
                 <button onClick={() =>setReply(!reply)} style={reply ? { display: 'none' } : {}}>Reply!</button>
             
             </div>
-
             {answerstoRender}
             
-            {/* {answers.events.map((answer) => {
-                console.log('answers', answers)
-                console.log('answer', answer)
-            return <div className='answer-box' >
-
-                <div className="answer-content">
-                <h2>{answer.body}</h2>
-               <Link onClick={() =>setSelectedUser()} className="answer-author" to={{ pathname: `/UserProfile/`, state: {selectedUser: answer.author, userAnswer: answer.body } }}><h3>{answer.author}</h3></Link>
-                
-                <LikeBtn />
-                
-                </div>
-
-                </div>
-                
-        })} */}
         </div>
- )
-}
+)}
 
 export default QuestionAnswers
